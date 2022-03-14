021a9366384f4e8a5ebbfb70fbdffc2023c4a91e
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-03-14 12:54:20 +0700

    beta test

---
+++ QueenIraa/modules/helper_funcs/telethn/__init__.py
@@ -0,0 +1,14 @@
+from QueenIraa import (
+    telethn,
+    SUDO_USERS,
+    WHITELIST_USERS,
+    SUPPORT_USERS,
+    GEEZRAM_USERS,
+    DEV_USERS,
+)
+
+HIGHER_AUTH = SUDO_USERS + DEV_USERS
+
+HIGHER_AUTH = list(SUDO_USERS) + list(DEV_USERS)
+
+HIGHER_AUTH.append(1087968824)
