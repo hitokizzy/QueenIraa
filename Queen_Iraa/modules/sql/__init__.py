2289889b87e4ad0969c02a7858f7779e08eb3ff6
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2022-03-15 21:28:19 +0700

    Update __init__.py

--- Queen_Iraa/modules/sql/__init__.py
+++ Queen_Iraa/modules/sql/__init__.py
@@ -31,7 +31,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from EmikoRobot import DB_URL
+
+#code from Queen Iraa
+DB_URL = os.getenv("DATABASE_URL")  # or other relevant config var
+if DB_URL and DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+
+
+BASE = declarative_base()
+SESSION = start()
 def start() -> scoped_session:
     engine = create_engine(DB_URL, client_encoding="utf8")
     BASE.metadata.bind = engine
